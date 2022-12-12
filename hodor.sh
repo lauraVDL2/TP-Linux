1df19ff3a8292a1ba76f86696f4ca29f7df2c5b7
lauraVDL2 <laura.vanderlinden@hotmail.com>
2022-12-12 08:06:29 -0500

    extension

--- hodor.sh
+++ hodor.sh
@@ -4,6 +4,6 @@ if ((100 - $size < 80))
 then
 	uuid=$(cat /proc/sys/kernel/random/uuid)
 	tar -cvf "mon_archive-${uuid}.tar.gz" var/log/exercice/recent_logs
-	mv "mon_archive-${uuid}" var/log/exercice/archives
+	mv "mon_archive-${uuid}.tar.gz" var/log/exercice/archives
 	wall "HODOOOR"
 fi
