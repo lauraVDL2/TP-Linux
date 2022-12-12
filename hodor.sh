8fa157a8d34b6091f39c0735d95fe215cb37b26e
lauraVDL2 <laura.vanderlinden@hotmail.com>
2022-12-12 07:59:52 -0500

    hodor.sh

---
+++ hodor.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+size=$(df -i / | cut -d " " -f 12 | tail -n 1 | tr -d '%')
+if ((100 - $size < 80))
+then
+	uuid=$(cat /proc/sys/kernel/random/uuid)
+	tar -cvf "mon_archive-${uuid}.tar.gz" var/log/exercice/recent_logs
+	mv "mon_archive-${uuid}" var/log/exercice/archives
+	wall "HODOOOR"
+fi
