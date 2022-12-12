11e95589e76160d3eb87e86080b45bb3f14e341e
lauraVDL2 <laura.vanderlinden@hotmail.com>
2022-12-12 10:39:01 -0500

    more corrections

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 characters=$(head -c 10 /dev/random)
-mkdir -p var/log/exercice/recent_logs
-cd var/log/exercice/recent_logs
+mkdir -p /var/log/exercice/recent_logs
+cd /var/log/exercice/recent_logs
 for i in 1 .. $1
 do
 	uuid=$(cat /proc/sys/kernel/random/uuid)
