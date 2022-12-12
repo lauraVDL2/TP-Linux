1e5908c89b3d4a5c8cc87724af57ff8b182c18bf
lauraVDL2 <laura.vanderlinden@hotmail.com>
2022-12-12 05:38:23 -0500

    log.sh

---
+++ log.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+characters=$(head -c 10 /dev/random)
+mkdir -p var/log/exercice/recent_logs
+cd var/log/exercice/recent_logs
+for i in 1 .. $1
+do
+	uuid=$(cat /proc/sys/kernel/random/uuid)
+	filename="monFichier-${uuid}"
+	touch $filename
+	echo $characters>$filename
+done
